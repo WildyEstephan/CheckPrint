d51d0568181531f02c7e1a02448a523af2a02e7d
Wildy Estephan <westephan50@gmail.com>
2022-03-03 06:56:01 -0400

    Add amount two

--- dev_print_cheque/models/cheque_setting.py
+++ dev_print_cheque/models/cheque_setting.py
@@ -69,6 +69,13 @@ class cheque_setting(models.Model):
 
     is_currency = fields.Boolean('Print Currency')
 
+    is_amount_two = fields.Boolean('Print Amount', default=True)
+    amt_m_top_two = fields.Float('Amt From Top', default=185)
+    amt_m_left_two = fields.Float('Amt From Left', default=550)
+    is_star_two = fields.Boolean('Print Star', help="if true then print 3 star before and after Amount", default=True)
+
+    is_currency_two = fields.Boolean('Print Currency')
+
     is_amount_word = fields.Boolean('Print Amount Words', default=True)
     is_word_bold = fields.Boolean('Word Font Bold')
     word_in_f_line = fields.Float('Split Words After', default=5,
